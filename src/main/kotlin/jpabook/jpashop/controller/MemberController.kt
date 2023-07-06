aa10af8731ca86bc56d7062491bb83c2e0a787c4
m5s3 <devmyoungsoo2@gmail.com>
2023-07-06 22:58:33 +0900

    회원 조회 화면 개발

--- src/main/kotlin/jpabook/jpashop/controller/MemberController.kt
+++ src/main/kotlin/jpabook/jpashop/controller/MemberController.kt
@@ -8,6 +8,7 @@ import jpabook.jpashop.service.MemberService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -16,7 +17,7 @@ class MemberController(
     private val memberService: MemberService,
 ) {
 
-    @RequestMapping("/members/new")
+    @GetMapping("/members/new")
     fun createForm(model: Model): String {
         model.addAttribute("memberForm", MemberForm())
         return "members/createMemberForm";
@@ -35,5 +36,12 @@ class MemberController(
         memberService.join(member)
 
         return "redirect:/"
+    }
+
+    @GetMapping("/members")
+    fun list(model: Model): String {
+        val members = memberService.findMembers()
+        model.addAttribute("members", members)
+        return "members/memberList"
     }
 }
