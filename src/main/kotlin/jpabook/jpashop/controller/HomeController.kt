57c6ab024449c008dfeb2b8cc58decc220ba5e83
m5s3 <devmyoungsoo2@gmail.com>
2023-07-06 23:20:30 +0900

    ->  으로 수정

--- src/main/kotlin/jpabook/jpashop/controller/HomeController.kt
+++ src/main/kotlin/jpabook/jpashop/controller/HomeController.kt
@@ -1,12 +1,13 @@
 package jpabook.jpashop.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     fun home(): String {
         return "home"
     }
