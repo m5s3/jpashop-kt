96e1fd045680dbb43996235516a63c2c4ed33a0d
m5s3 <devmyoungsoo2@gmail.com>
2023-07-06 21:44:39 +0900

    메인 컨트롤러 개발

---
+++ src/main/kotlin/jpabook/jpashop/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package jpabook.jpashop.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class HomeController {
+
+    @RequestMapping("/")
+    fun home(): String {
+        return "home"
+    }
+}
